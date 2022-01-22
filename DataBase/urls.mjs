42747471c08b06cb4ca31ee47a5ec08d405f7443
rishabh sahu <rishabhsahu106@gmail.com>
2022-01-22 20:12:53 +0530

    update port

--- DataBase/urls.mjs
+++ DataBase/urls.mjs
@@ -1,3 +1,3 @@
-const mongodburl = "mongodb://127.0.0.1:2701/bettersleep";
+const mongodburl = "mongodb://127.0.0.1:27017/bettersleep";
 const jwtkey = "bettersleepadmin"
 export {mongodburl,jwtkey};
